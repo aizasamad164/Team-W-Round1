a4d7c99295631f16a8da406b7ab2cda0b2370f7d
Aiza Samad Khan <113856618+aizasamad164@users.noreply.github.com>
2026-01-21 10:44:04 +0500

    Update Round1_Solution.py

--- Round1_Solution.py
+++ Round1_Solution.py
@@ -1,1 +1,24 @@
+import random 
+
+r = random.randint(1, 50)
+x = 1
+num = int(input("Enter a number between 1 and 50: "))
+
+while num != r:    
+    x += 1
+
+    if num > r:
+        print("That is above the target")
+    elif num < r: 
+        print("That is below the target")
+    else:
+        break
+
+    num = int(input("Enter a number between 1 and 50: "))
+
+print("Correct you guessed in", x, "attempts")
+        
+    
+    
+
 
